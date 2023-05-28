06dcb1adfd889540d32ae659bf370c74b240d43f
Phibeon <phibeon@yahoo.com>
2023-05-28 09:09:07 +0200

    added single page
    templates for single page
    mapped urls to views

--- pages/urls.py
+++ pages/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'pages'
 urlpatterns = [
     path('', views.index , name='index'),
+    path('<int:page_id>', views.page, name='blog'),
 ]
 
--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from .models import Blog
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def index(request):
     blogs = Blog.objects.all()
     return render(request, 'pages/index.html', {'blogs':blogs})
+
+def page(request, page_id):
+    blog = get_object_or_404(Blog, pk=page_id)
+    return render(request, 'pages/blog.html', {'blog':blog})
